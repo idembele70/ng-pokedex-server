92e1c02761c375429f2a8b33fef91f4d9fde5be0
Ibrahim Dembele <idembele70@gmail.com>
2025-11-17 17:23:21 +0100

    [ng-pokedex #24] Add get user liked pokemons endpoint (#3)
    * feat: add get user liked pokemons endpoint
    
    Refs: idembele70/ng-pokedex#24
    
    * change as per review
    
    Refs: idembele70/ng-pokedex#24
    
    ---------
    
    Co-authored-by: idembele70 <idembele70@gmail>

--- src/modules/pokemon/pokemon.routes.mjs
+++ src/modules/pokemon/pokemon.routes.mjs
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import PokemonController from './pokemon.controller.mjs';
+import { authMiddleware } from '../../core/middleware/auth.middleware.mjs';
 
 const router = Router();
 
 router
   .get('/', PokemonController.getAll)
-  .get('/search', PokemonController.filterAll);
+  .get('/search', PokemonController.filterAll)
+  .get('/likes', authMiddleware, PokemonController.filterLiked);
 
 export default router;
